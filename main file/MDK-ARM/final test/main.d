e0e12a32f6720fef49d837e0b35c3c91c5acfe32
Zahra Arabi <51692883+z-arabi@users.noreply.github.com>
2021-09-10 13:56:58 +0430

    complete

---
+++ main file/MDK-ARM/final test/main.d
@@ -0,0 +1,31 @@
+final test\main.o: ../Src/main.c
+final test\main.o: ../Inc/main.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+final test\main.o: ../Inc/stm32f1xx_hal_conf.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+final test\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+final test\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+final test\main.o: ../Drivers/CMSIS/Include/core_cm3.h
+final test\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+final test\main.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+final test\main.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+final test\main.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+final test\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+final test\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+final test\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+final test\main.o: ../RGB library/RGB_library.h
+final test\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+final test\main.o: ../RGB library/stm32f1xx_hal_tim.h
+final test\main.o: ../RGB library/stm32f1xx_hal_tim_ex.h
+final test\main.o: ../RGB library/stm32f1xx_hal_uart.h
+final test\main.o: ../RGB library/stm32f1xx_hal_dma.h
---
+++ main file/MDK-ARM/final test/rgb_library.d
@@ -0,0 +1,31 @@
+final test\rgb_library.o: ..\RGB library\RGB_library.c
+final test\rgb_library.o: ..\RGB library\RGB_library.h
+final test\rgb_library.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+final test\rgb_library.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+final test\rgb_library.o: ../Inc/stm32f1xx_hal_conf.h
+final test\rgb_library.o: ../Inc/main.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+final test\rgb_library.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+final test\rgb_library.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+final test\rgb_library.o: ../Drivers/CMSIS/Include/core_cm3.h
+final test\rgb_library.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+final test\rgb_library.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+final test\rgb_library.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+final test\rgb_library.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+final test\rgb_library.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+final test\rgb_library.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+final test\rgb_library.o: ..\RGB library\stm32f1xx_hal_tim.h
+final test\rgb_library.o: ..\RGB library\stm32f1xx_hal_tim_ex.h
+final test\rgb_library.o: ..\RGB library\stm32f1xx_hal_uart.h
+final test\rgb_library.o: ..\RGB library\stm32f1xx_hal_dma.h
